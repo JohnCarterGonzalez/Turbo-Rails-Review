4e4c7428efaa8c4b4b4f90a74182a2ef9bd4c8e4
JohnCarter <cascadedevelopment@proton.me>
2023-07-03 15:19:17 -0700

    first commit

--- app/channels/turbo_clone/streams_channel.rb
+++ app/channels/turbo_clone/streams_channel.rb
@@ -1,7 +1,10 @@
 class TurboClone::StreamsChannel < ActionCable::Channel::Base
-  extend TurboClone::Streams::StreamName, TurboClone::Streams::Broadcasts
+  # for info related to stream names, see ./stream_name.rb
+  extend TurboClone::Streams::StreamName
+  extend TurboClone::Streams::Broadcasts
 
   def subscribed
+    # accept the sub and stream from the passed params (in this case it will be articles)
     if verified_stream_name = self.class.verified_stream_name(params[:signed_stream_name])
       stream_from verified_stream_name
     else
